a188714521ae14a7f451fb9f6f2083baca96d3b3
suminhan123 <suminh324@gmail.com>
2024-07-30 11:39:48 +0900

    feat(test): 6 커버리지 리포트를 통해 테스트 개선 예제 추가

--- frontend-testing-book/06-커버리지 리포트 읽기/coverage/jest.config.ts
+++ frontend-testing-book/06-커버리지 리포트 읽기/coverage/jest.config.ts
@@ -1,41 +1,14 @@
-/* 코드 5-1
-module.exports = {
-  testEnvironment: "jest-environment-jsdom",
-};
-*/
-
-/* 코드 6-1
-export default {
-  // 생략
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-};
-*/
-
-/* 코드 6-7
-export default {
-  // 생략
-  reporters: [
-    "default",
-    [
-      "jest-html-reporters",
-      {
-        publicPath: "__reports__",
-        filename: "jest.html",
-      },
-    ],
-  ],
-};
-*/
-
 export default {
   clearMocks: true,
+  // 커버리지 옵션을 넣지 않아도 리포트가 생성된다
   collectCoverage: false,
   coverageDirectory: "coverage",
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
   testEnvironment: "jest-environment-jsdom",
   transform: { "^.+\\.(ts|tsx)$": ["esbuild-jest", { sourcemap: true }] },
   setupFilesAfterEnv: ["./jest.setup.ts"],
+  // 선호하는 리포트를 추가
+  // jest-html-reporters : 테스트 실행 결과를 그래프 형태로 보여준다
   reporters: [
     "default",
     [
