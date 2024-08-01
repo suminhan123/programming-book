f1181d5fc5abcbfe74fb0284f3a5a29caa12af12
suminhan123 <suminh324@gmail.com>
2024-08-01 10:18:41 +0900

    feat(test): 7.2 context를 통한 커스텀 훅을 활용한 Toast 컴포넌트 추가

---
+++ frontend-testing-book/07-웹 애플리케이션 통합 테스트/nextjs/src/components/providers/ToastProvider/index.tsx
@@ -0,0 +1,45 @@
+import { ReactNode } from "react";
+import { Toast } from "./Toast";
+import {
+  ToastActionContext,
+  ToastState,
+  ToastStateContext,
+  ToastStyle,
+} from "./ToastContext";
+import { useToastProvider } from "./useToastProvider";
+export { useToastAction, useToastState } from "./hooks";
+export type { ToastState, ToastStyle };
+
+/* 토스트 사용 예시
+const { showToast } = useToastAction();
+const onSubmit = handleSubmit(async () => {
+  try {
+    // ...웹 API에 값을 제출한다.
+    showToast({ message: "저장됐습니다", style: "succeed" });
+  } catch (err) {
+    showToast({ message: "에러가 발생했습니다", style: "failed" });
+  }
+});
+*/
+
+export const ToastProvider = ({
+  children,
+  defaultState,
+}: {
+  children: ReactNode;
+  defaultState?: Partial<ToastState>;
+}) => {
+  const { isShown, message, style, showToast, hideToast } =
+    useToastProvider(defaultState);
+  return (
+    <ToastStateContext.Provider value={{ isShown, message, style }}>
+      {/* 하위 컴포넌트에서 isShown, message, style을 참조할 수 있다 */}
+      <ToastActionContext.Provider value={{ showToast, hideToast }}>
+        {/* 하위 컴포넌트에서 showToast, hideToast를 참조할 수 있다 */}
+        {children}
+        {isShown && <Toast message={message} style={style} />}
+        {/* isShown이 true가 되면 표시된다 */}
+      </ToastActionContext.Provider>
+    </ToastStateContext.Provider>
+  );
+};
